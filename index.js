89555f8f769b2f2feeefa7a7bf95818f4d3590ef
drewthoennes <dthoenne@purdue.edu>
2018-08-01 22:37:44 -0400

    Adds /dist for Heroku

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ require('./server/routes/error.js')(app);
 // Frontend endpoints
 app.use(express.static(__dirname + "/dist"));
 app.use('/', express.static(__dirname + "/dist"));
+//app.use('/documentation', express.static(__dirname + "/dist"));
+//app.use('/about', express.static(__dirname + "/dist"));
+
 // Catch all for frontend
 app.all('/*', function(req, res) {
 		res.sendFile(path.join(__dirname, '/dist', '/index.html'));
